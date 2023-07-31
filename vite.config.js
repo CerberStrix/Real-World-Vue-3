c41590ac8f09e0922f3fdc38e42ea294f8ed0457
Alisher Kim <cerberstrix@gmail.com>
2023-07-31 20:11:19 +0500

    add all

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -8,7 +7,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-    viteCommonjs(),
   ],
   resolve: {
     alias: {
