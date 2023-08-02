c5499edafec910c26beb80c946a1e73513b21000
Alisher Kim <cerberstrix@gmail.com>
2023-08-02 20:00:04 +0500

    add axios alternative way

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm';
 
 const apiClient = axios.create({
   baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
